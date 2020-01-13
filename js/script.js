2d8d3e1777c9ec2770a10ee54b04b22025818d9f
Matteo <matteop18@gmail.com>
2020-01-13 15:05:17 +0100

    split

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
 // Scrivi una funzione per capire se la parola è palindroma
+// Creare un array contenente tutti i singoli caratteri della stringa inserita dall'utente
+
+// Metodo Split()
+var parolaUtente = prompt('Inserisci una parola');
+
+var split = parolaUtente.split('');
+console.log(split);

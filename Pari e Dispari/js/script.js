589a67c7996bbccbb500cae812505816388d17e9
Matteo <matteop18@gmail.com>
2020-01-13 16:49:19 +0100

    da eseguire con funzione

--- Pari e Dispari/js/script.js
+++ Pari e Dispari/js/script.js
@@ -13,13 +13,25 @@ var numRandom = Math.floor(Math.random() * 5) + 1;
 var somma = numRandom + numeroUtente;
 
 var risultatoFinale = false;
+console.log(numeroUtente);
+console.log(numRandom);
+console.log(somma);
 
-if (somma % 2 == 0) {
+if (somma % 2 == 0 ) {
   risultatoFinale = true;
 } else {
   risultatoFinale = false;
 }
 
 console.log(risultatoFinale);
-console.log(numRandom);
-console.log(somma);
+
+
+if (pariDispari == 'pari' && risultatoFinale == true) {
+  console.log('Hai vinto');
+} else if (risultatoFinale == true && pariDispari == 'dispari')  {
+  console.log(' Hai perso');
+} else if (risultatoFinale == false && pariDispari == 'dispari'){
+  console.log('Hai vinto');
+} else if (risultatoFinale == false && pariDispari == 'pari') {
+  console.log('Hai perso');
+}

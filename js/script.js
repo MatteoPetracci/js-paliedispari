5afbefc12b0af7ecb9fc289157d7538e9e3373ad
Matteo <matteop18@gmail.com>
2020-01-13 15:18:16 +0100

    prova con funzione

--- js/script.js
+++ js/script.js
@@ -1,40 +1,63 @@
 //  Scrivi una funzione per capire se la parola è palindroma
 //  Creare un array contenente tutti i singoli caratteri della stringa inserita dall'utente
 
-//  Metodo Split()
-var parolaUtente = prompt('Inserisci una parola');
+// var parolaUtente = prompt('Inserisci una parola');
 //
-var split = parolaUtente.split('');
-console.log(split);
+// //  Metodo Split()
+//
+// var split = parolaUtente.split('');
+// console.log(split);
+//
+// //  invertire la disposizione degli elementi che compongono l' array
+//
+// // Metodo Reverse()
+//
+// var reverse = split.reverse();
+// console.log(reverse);
+//
+// //  Riunire gli elementi di un array in una stringa.
+//
+// //  Metodo join()
+//
+// var join = reverse.join('');
+// console.log(join);
+//
+// var parola = false;
+//
+// //  Recuperare un valore booleano
+//
+// if(parolaUtente == join) {
+//   parola = true;
+// } else {
+//   parola = false;
+// }
+// console.log(parola);
+//
+// if (parola == true) {
+//   console.log('Questa parola è polindroma');
+// } else {
+//   console.log('Questa parola non è polindroma');
+// }
 
 
-//  invertire la disposizione degli elementi che compongono l' array
+var parolaUtente = prompt('Inserisci una parola');
 
-// Metodo Reverse()
 
-var reverse = split.reverse();
-console.log(reverse);
-
-//  Riunire gli elementi di un array in una stringa.
-
-//  Metodo join()
-
-var join = reverse.join('');
-console.log(join);
-
-var parola = false;
-
-//  Recuperare un valore booleano
-
-if(parolaUtente == join) {
-  parola = true;
-} else {
-  parola = false;
+function polindroma(parola) {
+  var split = parolaUtente.split('');
+  var reverse = split.reverse();
+  var join = reverse.join('');
+  if(parolaUtente == join) {
+    return true;
+  } else {
+    return false;
+  }
 }
-console.log(parola);
 
-if (parola == true) {
-  console.log('Questa parola è polindroma');
+var polindromaParola = polindroma(parolaUtente);
+
+if (polindromaParola == true) {
+   console.log('Questa parola è polindroma');
 } else {
-  console.log('Questa parola non è polindroma');
+  console.log('Questa parola non è polidroma');
 }
